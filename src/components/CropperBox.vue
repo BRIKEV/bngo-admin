e17d34071bad817b213fa2769e47058b0f3c8f6f
kevinccbsg <kevinccbsg@gmail.com>
2022-03-12 20:19:07 +0100

    feat: add basic cropper

---
+++ src/components/CropperBox.vue
@@ -0,0 +1,36 @@
+<template>
+  <cropper
+    class="cropper"
+    :src="image.src"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+import { Cropper } from "vue-advanced-cropper";
+import "vue-advanced-cropper/dist/style.css";
+
+export default defineComponent({
+  components: {
+    Cropper,
+  },
+
+  setup() {
+    const image = reactive({
+      src:
+        "https://images.unsplash.com/photo-1619737307100-55b82496fcda?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+      type: "image/jpg",
+    });
+
+    return {
+      image,
+    };
+  },
+});
+</script>
+
+<style>
+.cropper {
+  max-height: 500px;
+}
+</style>
